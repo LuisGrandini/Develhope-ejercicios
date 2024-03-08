5ecf0ecd8195ebfe0fedf2f1034a1303c157dbbf
LuisGrandini <grandiniluis18@gmail.com>
2024-03-08 18:19:44 +0100

    template string

--- css/java/ej33.js
+++ css/java/ej33.js
@@ -1,3 +1,5 @@
 class Person { constructor(firstName, lastName) { this.firstName = firstName; this.lastName = lastName; } }
 
-const developer = new Person("Mario", "Rossi"); console.log(developer.firstName + " " + developer.lastName); 
+const developer = new Person("Mario", "Rossi"); 
+
+console.log(`${developer.firstName} ${developer.lastName}`); 
